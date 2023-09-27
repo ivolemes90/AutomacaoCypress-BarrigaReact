00ca7c3437b45b0d76fc203e48ddfffb2f4a681e
Ivo Lemes <ivolemes.dev@gmail.com>
2023-09-27 20:34:19 -0300

    Adicionando novos testes funcionais, novos locators e funções

--- cypress/e2e/Funcional.cy.js
+++ cypress/e2e/Funcional.cy.js
@@ -1,33 +1,65 @@
 ///<reference types="cypress"/>
 
+import loc from "../support/locators"
+import "../support/commandsContas"
+
 describe('Should test at a functional level', () => {
-    before(() => {
-        cy.visit('https://barrigareact.wcaquino.me')
-        cy.get('.input-group > .form-control').type('ivo@gmail.com')
-        cy.get(':nth-child(2) > .form-control').type('ivo@123')
-        cy.get('.btn').click()
-        cy.get('.toast-message').should('contain', 'Bem vindo')
+    beforeEach(() => {
+        cy.login('ivo@gmail.com', 'ivo@123')  
+    })
+    
+    it('Should reset the app', ()=> {
+        cy.resetApp()
+        cy.get(loc.MESSAGE).should('contain', 'Dados resetados com sucesso!')
+
     })
 
     it('Should creatre an account', () => {
-        cy.get('[data-test="menu-settings"]').click()
-        cy.get('[href="/contas"]').click()
-        cy.get('[data-test="nome"]').type('Conta de teste2')
-        cy.get('.btn').click()
-        cy.get('.toast-success > .toast-message').should('contain', 'Conta inserida com sucesso!')
+        cy.acessarMenuConta()
+        cy.inserirConta('Conta de teste')
+        cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso!')
+    
+    })
+
+    it('Should update an account', () =>{
+        cy.acessarMenuConta()
+        cy.xpath(loc.CONTAS.FN_XP_BTN_ALTERAR('Conta de teste')).click()
+        cy.get(loc.CONTAS.NOME)
+            .clear()
+            .type('Conta de teste alterada')
+        cy.get(loc.CONTAS.BTN_CT_SALVAR).click()
+        cy.get(loc.MESSAGE).should('contain', 'Conta atualizada com sucesso!')
+      
+    })
+
+    
+    it('Should not create an account with same name', ()=>{
+        cy.acessarMenuConta()
+        cy.inserirConta('Conta de teste alterada')
+        cy.get(loc.MESSAGE).should('contain', 'code 400')
 
     })
 
-    it.only('Should update an account', () =>{
-        cy.get('[data-test="menu-settings"]').click()
-        cy.get('[href="/contas"]').click()
-        cy.xpath("//table//td[contains(.,'Conta de teste')]/..//i[@class='far fa-edit']").click()
-        cy.get('[data-test="nome"]')
-            .clear()
-            .type('Conta de teste1')
-        cy.get('.btn').click()
-        cy.get('.toast-message').should('contain', 'Conta atualizada com sucesso!')
-    
+    it('Should create a transation', ()=>{
+        cy.get(loc.MENU.MOVIMENTACAO).click()
+
+        cy.get(loc.MOVIMENTACAO.DESCRICAO).type('Desc')
+        cy.get(loc.MOVIMENTACAO.VALOR).type('123')
+        cy.get(loc.MOVIMENTACAO.INTERESSADO).type('Inter')
+        cy.get(loc.MOVIMENTACAO.CONTA).select('Conta de teste alterada')
+        cy.get('[data-test="status"]').click()
+        cy.get(loc.MOVIMENTACAO.BTN_MVT_SALVAR).click()
+        cy.get(loc.MESSAGE).should('contain', 'Movimentação inserida com sucesso!')
+
+        cy.get(loc.EXTRATO.LINHAS).should('have.length', 7)
+        cy.xpath(loc.EXTRATO.FN_XP_BUSCA_ELEMENTO('Desc', '123')).should('exist')
         
     })
+
+    it('Should get balance', ()=> {
+        cy.get(loc.MENU.HOME).click()
+        cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta de teste alterada')).should('contain', '123,00')
+
+    })
 })
+ 
---
+++ cypress/support/locators.js
@@ -0,0 +1,37 @@
+const locators = {
+    LOGIN: {
+        USER: '[data-test="email"]',
+        PASSWORD: '[data-test="passwd"]',
+        BTN_LOGIN: '.btn'
+    },
+    MENU: {
+        HOME: '[data-test="menu-home"] > .fas',
+        SETTINGS: '[data-test="menu-settings"]',
+        CONTAS: '[href="/contas"]',
+        RESET: '[href="/reset"]',
+        MOVIMENTACAO:'[data-test="menu-movimentacao"]'
+    },
+    CONTAS: {
+        NOME: '[data-test="nome"]',
+        BTN_CT_SALVAR: '.btn',
+        FN_XP_BTN_ALTERAR: nome => `//table//td[contains(.,'${nome}')]/..//i[@class='far fa-edit']`
+    },
+    MOVIMENTACAO: {
+        DESCRICAO: '[data-test="descricao"]',
+        VALOR: '[data-test="valor"]',
+        INTERESSADO: '[data-test="envolvido"]',
+        CONTA: '[data-test="conta"]',
+        STATUS: '[data-test="status"]',
+        BTN_MVT_SALVAR: '.btn-primary'
+    },
+    EXTRATO: {
+        LINHAS: '.list-group > li',
+        FN_XP_BUSCA_ELEMENTO: (desc, value) => `//span[contains(.,'${desc}')]/..//small[contains(.,'${value}')]`
+    },
+    SALDO: {
+        FN_XP_SALDO_CONTA: NOME => `//td[contains(.,'${NOME}')]/..//td[2]`
+    },
+    MESSAGE: '.toast-message'
+}
+
+export default locators;
